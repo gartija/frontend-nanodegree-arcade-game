ddc5d02c3f77c21a702bd2392c9e070266f16dea
Francisco Yemail <lyemail@H8240.ad.hugeinc.com>
2018-04-28 16:12:07 -0500

    Implemented basic methods for Player and Enemy classes so that the game gets rendered with a static enemy and player

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Enemies our player must avoid
 var Enemy = function() {
+    this.x = 0;
+    this.y = 0;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -24,12 +26,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 100;
+    this.y = 100;
+    this.sprite = 'images/char-boy.png'
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.handleInput = function(keyCode) {
+    console.log(keyCode);
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+allEnemies = [];
+allEnemies[0] = new Enemy();
 // Place the player object in a variable called player
-
+player = new Player();
 
 
 // This listens for key presses and sends the keys to your
